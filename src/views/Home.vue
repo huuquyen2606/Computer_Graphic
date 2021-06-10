68d40d8186fcfccab473ef57b8bee04018354d36
huuquyen2606 <huuquyen2606@gmail.com>
2021-06-10 16:57:58 +0700

    checking

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Computer Graphic Project"/> -->
+    <Scene/>
+    <div>alolololololo</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  // import HelloWorld from '@/components/HelloWorld.vue'
+  import Scene from '@/components/Scene.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      // HelloWorld,
+      Scene
+    }
   }
-}
 </script>
